4025ebd2bc59e36eb8130afa1f1588d474ec64b6
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-11-22 08:58:34 -0800

    tried exercise

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// YOUR JS CODE HERE
+// async function getWeather() {
+//     const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=49.2497&longitude=-123.1193&current=temperature_2m,is_day,rain,showers,wind_speed_10m&timezone=auto&forecast_days=1")
+//     const data = await response.json()
+//     return data
+// }
+
+
+
+
+ 
+const section = document.querySelector(".Weather")
+
+const getWeather = async () =>{
+    let response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=49.2497&longitude=-123.1193&current=temperature_2m,is_day,rain,showers,wind_speed_10m&timezone=auto&forecast_days=1")
+    const data = await response.json()
+    return data
+}
+
+
+const loadData = async () =>{
+    const valuesWeather = await getWeather()
+    console.log(valuesWeather)
+    section.innerHTML = `
+        <h3>${valuesWeather.current.temperature_2m}${valuesWeather.current_units.temperature_2m}</h3>
+    `
+
+}
+
+loadData()
